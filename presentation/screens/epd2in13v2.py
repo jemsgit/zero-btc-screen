63f086c4d9ccd3aa2f0bb65a85157f30cddd0493
Enveniy Martynov <jemguns@gmail.com>
2021-05-13 17:49:07 +0100

    delete temp code

--- presentation/screens/epd2in13v2.py
+++ presentation/screens/epd2in13v2.py
@@ -23,11 +23,6 @@ class Epd2in13v2(Observer):
         self.screen_draw = ImageDraw.Draw(self.screen_image)
         self.mode = mode
 
-    def draw_loading(self):
-        img = Image.new('RGB', (100, 30), color = (73, 109, 137))
-        d = ImageDraw.Draw(img)
-        d.text((10,10), "Hello World", fill=(255,255,0))
-
     @staticmethod
     def _init_display(epd):
         epd.init(epd.FULL_UPDATE)
